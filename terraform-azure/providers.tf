89acbacf407f0c7763acb323d97581a28f75b958
Vignesh V S <vsvignesh63852@gmail.com>
2025-10-11 08:28:08 +0530

    Update providers.tf

--- terraform-azure/providers.tf
+++ terraform-azure/providers.tf
@@ -1,10 +1,24 @@
 terraform {
   required_version = ">= 1.3.0"
   required_providers {
-    azurerm = { source = "hashicorp/azurerm", version = "~> 3.113" }
-    random  = { source = "hashicorp/random",  version = "~> 3.6" }
-    local   = { source = "hashicorp/local",   version = "~> 2.5" }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.113"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5"
+    }
   }
 }
-provider "azurerm" { features {} }
+
+provider "azurerm" {
+  features {}
+}
+
+
 
