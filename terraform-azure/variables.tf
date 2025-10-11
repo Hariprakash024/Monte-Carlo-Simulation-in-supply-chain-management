1f5760e482e40b7f2c74cdee7e1d3158f04ebc5e
Vignesh V S <vsvignesh63852@gmail.com>
2025-10-11 08:24:52 +0530

    Modify Azure variables for location and repo URL
    Updated the default location to 'centralindia' and modified the repo_url to a new GitHub repository.

--- terraform-azure/variables.tf
+++ terraform-azure/variables.tf
@@ -1,7 +1,36 @@
-variable "location" { type=string, default="eastus", description="Azure region" }
-variable "resource_group_name_prefix" { type=string, default="mc-rg" }
-variable "admin_username" { type=string, default="azureuser" }
-variable "ssh_public_key_path" { type=string, default="~/.ssh/id_rsa.pub" }
-variable "vm_size" { type=string, default="Standard_B1s" }
-variable "vm_count" { type=number, default=2 }
-variable "repo_url" { type=string, default="https://github.com/your-user/your-repo.git" }  # CHANGE THIS
+variable "location" {
+  type        = string
+  default     = "centralindia"
+  description = "Azure region (e.g., centralindia, southindia, eastus)"
+}
+
+variable "resource_group_name_prefix" {
+  type    = string
+  default = "mc-rg"
+}
+
+variable "admin_username" {
+  type    = string
+  default = "azureuser"
+}
+
+variable "ssh_public_key_path" {
+  type    = string
+  default = "~/.ssh/id_rsa.pub"
+}
+
+variable "vm_size" {
+  type    = string
+  default = "Standard_B1s"
+}
+
+variable "vm_count" {
+  type    = number
+  default = 2
+}
+
+variable "repo_url" {
+  type        = string
+  default     = "https://github.com/Hariprakash024/Monte-Carlo-Simulation-in-supply-chain-management.git"
+  description = "Public GitHub repo; main has src/mc_mpi_cpu.cpp"
+}
